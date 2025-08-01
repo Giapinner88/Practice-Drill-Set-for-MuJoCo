c52c658578e46235d45a18f8649dfb159ad03600
Giapinner88 <giapwork98@gmail.com>
2025-08-01 13:50:34 +0700

    Frist commit

---
+++ Launch_Ex.py
@@ -0,0 +1,20 @@
+import mujoco
+import mujoco.viewer
+import time
+
+model = mujoco.MjModel.from_xml_path("Practise/02_StaticBoxGeom/model.xml")
+data = mujoco.MjData(model)
+
+# Launch viewer
+with mujoco.viewer.launch_passive(model, data) as viewer:
+    mujoco.mj_resetData(model, data)
+
+    start = time.time()
+    while viewer.is_running():
+        step_start = time.time()
+
+        mujoco.mj_step(model, data)
+        viewer.sync()
+
+        # Đảm bảo đúng tốc độ thực tế
+        time.sleep(max(0, 0.01 - (time.time() - step_start)))
